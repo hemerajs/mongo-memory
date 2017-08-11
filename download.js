0f4bdaa47de38133023a5e85ba62d15d1d97c121
Dustin Deus <deusdustin@gmail.com>
2017-08-11 14:47:54 +0200

    ....

--- download.js
+++ download.js
@@ -6,6 +6,8 @@ mongodHelper.mongoDBDownload.downloadAndExtract()
 
 .then((downloadLocation) => {
   console.log(`Downloaded MongoDB: ${downloadLocation}`)
+  process.exit(0)
 }, (err) => {
-  throw err
+  console.error(err)
+  process.exit(1)
 })
