bf2e996bd9587782ea01fc09b7122e92ad8a5482
Dustin Deus <deusdustin@gmail.com>
2017-08-11 14:25:01 +0200

    ..

--- download.js
+++ download.js
@@ -1,13 +1,11 @@
 const MongodbPrebuilt = require('mongodb-prebuilt')
-const Mkdirp = require('mkdirp')
 
-Mkdirp.sync('./tempdb')
-let mongodHelper = new MongodbPrebuilt.MongodHelper(['--port', '27018', '--dbpath', './tempdb', '--storageEngine', 'ephemeralForTest'])
+let mongodHelper = new MongodbPrebuilt.MongoDBPrebuilt()
 
-mongodHelper.run().then(() => {
-  console.log('mongodb downloaded')
-  process.exit(0)
-}).catch((err) => {
-  console.error(err)
-  process.exit(1)
+mongodHelper.mongoDBDownload.download()
+
+.then((downloadLocation) => {
+  console.log(`Downloaded MongoDB: ${downloadLocation}`)
+}, (err) => {
+  throw err
 })
